8509b05e5d09ca6f1d762d7de5c2469d67dcbc19
ox-prolifics <ox@prolifics.com>
2018-12-02 23:09:49 -0600

    rem files
    removing files

--- scripts/hpa-load.sh
+++
@@ -1,7 +0,0 @@
-#Get App URL and generate load using curl
-ROUTE="http://"$(oc get route hello-openshift --template "{{ .spec.host }}")
-for time in {1..15000}
-do
-  echo time $time
-  curl ${ROUTE}
-done
