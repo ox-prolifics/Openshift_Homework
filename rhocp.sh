82d517f8faa1d980767f0b63132ecb562ac16930
ox-prolifics <ox@prolifics.com>
2018-12-04 11:40:21 -0600

    draft3

--- rhocp.sh
+++ rhocp.sh
@@ -3,12 +3,12 @@
 chmod +x scripts/*
 temp=$1
 if [ "$1" = "uninstall" ]; then
-    ansible-playbook /root/rhocp_homework/yaml/cleanup.yaml
+    ansible-playbook /root/rhocp_homework/yaml/cleanup.yaml | tee /var/log/rhocp_uninstall.log
 elif [ "$1" = "install" ]; then
-   ansible-playbook /root/rhocp_homework/yaml/homework.yaml
+   ansible-playbook /root/rhocp_homework/yaml/homework.yaml | tee /var/log/rhocp_install.log
 elif [ "$1" = "clean" ]; then
-   ansible-playbook /root/rhocp_homework/yaml/cleanup.yaml
-   ansible-playbook /root/rhocp_homework/yaml/homework.yaml
+   ansible-playbook /root/rhocp_homework/yaml/cleanup.yaml | tee /var/log/rhocp_uninstall.log
+   ansible-playbook /root/rhocp_homework/yaml/homework.yaml | tee /var/log/rhocp_install.log
 else
    echo "please provide option: uninstall, install, or clean"
 fi
